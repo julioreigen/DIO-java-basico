9394472508666cef63114c686f20a191cababc00
Julio <juliobrito.tec@gmail.com>
2023-08-23 16:45:48 -0300

    Add second section of the course

---
+++ src/edu/julio/secondsection/VariableTypes.java
@@ -0,0 +1,19 @@
+package edu.julio.secondsection;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        // Numbers
+        byte age = 123;
+        short year = 2023;
+        int zipCode = 4967000;
+        long id = 98765432109L;
+        float PI = 3.14f;
+        double salary = 2311.33;
+        // Characters
+        char gender = 'M';
+        String name = "Julio César";
+        // Boolean
+        boolean isAlive = true;
+
+    }
+}
