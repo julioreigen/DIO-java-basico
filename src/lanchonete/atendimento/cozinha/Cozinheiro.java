25753ee88cd7bcb9323d943af1dede910873cf24
Julio <juliobrito.tec@gmail.com>
2023-09-25 15:23:47 -0300

    Add files for Basic OOP course

---
+++ src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -0,0 +1,47 @@
+package lanchonete.atendimento.cozinha;
+
+import lanchonete.atendimento.Atendente;
+
+public class Cozinheiro {
+    public void adicionarLancheNoBalcao(){
+        System.out.println("Adicionando lanche natural no balcao");
+    }
+    public void adicionarSucoNoBalcao(){
+        System.out.println("Adicionando suco no balcao");
+    }
+    public void adicionarComboNoBalcao(){
+        adicionarLancheNoBalcao();
+        adicionarSucoNoBalcao();
+    }
+    private void prepararLanche(){
+        System.out.println("Preparando lanche natural");
+    }
+    private void prepararVitamina(){
+        System.out.println("Preparando suco");
+    }
+    private void prepararCombo(){
+        prepararLanche();
+        prepararVitamina();
+    }
+    private void selecionarIngredientesLanche(){
+        System.out.println("Selecionado o pão, salada, ovo e carne");
+    }
+    private void selecionarIngredientesVitamina(){
+        System.out.println("Selecionado o leite, fruta e suco");
+    }
+    private void lavarIngredientes(){
+        System.out.println("Lavando ingredientes");
+    }
+    private void baterVitaminaLiquidificador() {
+        System.out.println("BATENDO VITAMINA NO LIQUIDIFICADOR");
+    }
+    private void fritarIngredientesLanche() {
+        System.out.println("FRITANDO A CARNE E OVO PARA O HAMBÚRGUER");
+    }
+    void pedirParaTrocarGas(Almoxarife meuAmigo) {
+        meuAmigo.trocarGas();
+    }
+    private void pedirIngredientes(Almoxarife almoxarife) {
+        almoxarife.entregarIngredientes();
+    }
+}
